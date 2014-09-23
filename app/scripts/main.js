a9d9e063db3836112da902a382ff82f453f8fc31
kseniya-varabei <kseniya.varabei@gmail.com>
2014-09-23 14:29:25 +0300

    add rs-site

---
+++ app/scripts/main.js
@@ -0,0 +1,20 @@
+window.onload = function(){
+    var el = document.getElementById("nav");
+    var navBtn = document.getElementById("nav-btn");
+
+    window.toggleClass = function() {
+        if (el.className == "vertical-mode-closed") {
+            el.className = "vertical-mode";
+            navBtn.innerHTML = 'x';
+        } else {
+            el.className = "vertical-mode-closed";
+            navBtn.innerHTML = '&#x2261;';
+        }
+    }
+
+    window.onresize = function () {
+        el.className = "vertical-mode-closed";
+        navBtn.innerHTML = '&#x2261;';
+    }
+
+}
