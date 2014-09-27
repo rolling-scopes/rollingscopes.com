5e72fe0d43b4e4a63decb30477fd282a43807fef
Kseniya Varabei <kseniya.varabei@gmail.com>
2014-09-26 23:44:37 -0700

    Update pages
    Update schedule, add "call for speakers" section, update "venue
    section", update datastore.js

--- contributors/dataStore.js
+++ contributors/dataStore.js
@@ -1,386 +1,408 @@
 var dataStore = [
-    {
-        "name": "Alexander Gerasimov",
-        "talks": [
-            {
-                "title": "RS#2 - Front-end project assets management"
-            },
-            {
-                "title": "RS#3 - SpainJS"
-            },
-            {
-                "title": "RSW#1 - TDD"
-            },
-            {
-                "title": "RSW#2 - WebSocket Challenge"
-            },
-            {
-                "title": "RS#4 - Testing JavaScript Applications"
-            },
-            {
-                "title": "RS#7 - RetroSpective"
-            },
-            {
-                "title": "RS#8 - Keep calm & attend events: LondonJs"
-            }
-        ],
-        "imagePath": "alexander_gerasimov.jpg"
-    },
-    {
-        "name": "Alex Karlovich",
-        "talks": [
-            {
-                "title": "RS#2 - <video> Хроника"
-            },
-            {
-                "title": "RS#3 - SpainJS"
-            },
-            {
-                "title": "RS#7 - RetroSpective"
-            },
-            {
-                "title": "RS#8 - Stylish Stylus"
-            }
-        ],
-        "imagePath": "alex_karlovich.jpg"
-    },
-    {
-        "name": "Andre Gloukhmantchouk",
-        "talks": [
-            {
-                "title": "RS#2 - Front-end project assets management"
-            },
-            {
-                "title": "RS#3 - SpainJS"
-            },
-            {
-                "title": "RS#5 - Going To Interview: Best Practices"
-            },
-            {
-                "title": "RS#7 - RetroSpective"
-            }
-        ],
-        "imagePath": "andre_gloukhmantchouk.jpg"
-    },
-    {
-        "name": "Dzmitry Varabei",
-        "talks": [
-            {
-                "title": "RS#1 - Chrome DevTools"
-            },
-            {
-                "title": "RS#3 - SpainJS"
-            },
-            {
-                "title": "RS#3 - Chrome Internals: Paint and Composition"
-            },
-            {
-                "title": "RS#7 - RetroSpective"
-            }
-        ],
-        "imagePath": "dzmitry_varabei.jpg"
-    },
-    {
-        "name": "Siarhei Mikhailau",
-        "talks": [
-            {
-                "title": "RS#2 - HTML-coder never dies"
-            },
-            {
-                "title": "RS#3 - Graceful Degradation and Progressive Enhancement"
-            },
-            {
-                "title": "RS#4 - Popups - breaking the habits"
-            },
-            {
-                "title": "RS#6 - Hello, Data Binding!"
-            }
-        ],
-        "imagePath": "siarhei_mikhailau.jpg"
-    },
-    {
-        "name": "Aliaksei Shitkin",
-        "talks": [
-            {
-                "title": "RS#1 - Web Components"
-            },
-            {
-                "title": "RS#2 - WebComponents in action"
-            },
-            {
-                "title": "RS#9 - JavaScript Linters"
-            }
-        ],
-        "imagePath": "aliaksei_shitkin.jpg"
-    },
-    {
-        "name": "Konstantin Krivlenia",
-        "talks": [
-            {
-                "title": "RS#4 - JavaScript Must Die!"
-            },
-            {
-                "title": "RS#7 - JavaScript Virtual Machines"
-            },
-            {
-                "title": "RS#10 - DOM sweet DOOM"
-            }
-        ],
-        "imagePath": "konstantin_krivlenia.jpg"
-    },
-    {
-        "name": "Andrei Palchys",
-        "talks": [
-            {
-                "title": "RS#3 - Asm.js"
-            },
-            {
-                "title": "RS#7 - WebKit Project Overview"
-            }
-        ],
-        "imagePath": "andrei_palchys.jpg"
-    },
-    {
-        "name": "Dzmitry Gerasimov",
-        "talks": [
-            {
-                "title": "RS#4 - Testing JavaScript Applications"
-            },
-            {
-                "title": "RSW#1 - TDD"
-            }
-        ],
-        "imagePath": "dzmitry_gerasimov.jpg"
-    },
-    {
-        "name": "Kirill Kukhovets",
-        "talks": [
-            {
-                "title": "RS#5 -Welcome to Node.js"
-            },
-            {
-                "title": "RS#7 - Data processing with ql.io"
-            }
-        ],
-        "imagePath": "kirill_kukhovets.jpg"
-    },
-    {
-        "name": "Viktor Khomyakov",
-        "talks": [
-            {
-                "title": "RS#5 - Seamless and partial page reload"
-            },
-            {
-                "title": "RS#11 - Интеграция React в проект на RequareJS"
-            }
-        ],
-        "imagePath": "viktor_khomyakov.jpg"
-    },
-    {
-        "name": "Aleh Zasypkin",
-        "talks": [
-            {
-                "title": "RS Q&A #2 - How Mozilla Works"
-            }
-        ],
-        "imagePath": "aleh_zasypkin.jpg"
-    },
-    {
-        "name": "Andrew Fan",
-        "talks": [
-            {
-                "title": "RS#1 - Introduction to Ember"
-            }
-        ],
-        "imagePath": "andrew_fan.jpg"
-    },
-    {
-        "name": "Artsiom Aliakseyenka",
-        "talks": [
-            {
-                "title": "RS#11 - Memory Profiling"
-            }
-        ],
-        "imagePath": "artsiom_aliakseyenka.jpg"
-    },
-    {
-        "name": "Denis Shamgin",
-        "talks": [
-            {
-                "title": "RS#1 - Introduction to Angular.js"
-            }
-        ],
-        "imagePath": "denis_shamgin.jpg"
-    },
-    {
-        "name": "Dmitry Dudin",
-        "talks": [
-            {
-                "title": "RS#9 - Экзотические и полезные web API"
-            }
-        ],
-        "imagePath": "dmitry_dudin.jpg"
-    },
-    {
-        "name": "Dzianis Sheka",
-        "talks": [
-            {
-                "title": "RS#10 - Назад в будущее, или мобильная разработка сегодня"
-            }
-        ],
-        "imagePath": "dzianis_sheka.jpg"
-    },
-    {
-        "name": "Egor Miasnikov",
-        "talks": [
-            {
-                "title": "RS#9 - Google Glass"
-            }
-        ],
-        "imagePath": "egor_miasnikov.jpg"
-    },
-    {
-        "name": "Evgeny Zarya",
-        "talks": [
-            {
-                "title": "RS#4 - JavaScript: Non-classical inheritance"
-            }
-        ],
-        "imagePath": "evgeny_zarya.jpg"
-    },
-    {
-        "name": "Kiryl Baranosnik",
-        "talks": [
-            {
-                "title": "RS#5 -Taming AngularJs: A Directive Lifecycle"
-            }
-        ],
-        "imagePath": "kiryl_baranosnik.jpg"
-    },
-    {
-        "name": "Kiryl Verkhovin",
-        "talks": [
-            {
-                "title": "RSW#3 - Обработки и визуализация данных с помощью библиотеки D3.js на примере создания интерактивной карты Беларуси"
-            }
-        ],
-        "imagePath": "kiryl_verkhovin.jpg"
-    },
-    {
-        "name": "Maksim Chemerisuk",
-        "talks": [
-            {
-                "title": "RS#8 - Some fresh ideas from better-dom"
-            }
-        ],
-        "imagePath": "maksim_chemerisuk.jpg"
-    },
-    {
-        "name": "Mikhail Larchanka",
-        "talks": [
-            {
-                "title": "RS#9 - Google Glass"
-            }
-        ],
-        "imagePath": "mikhail_larchanka.jpg"
-    },
-    {
-        "name": "Mikita Manko",
-        "talks": [
-            {
-                "title": "RS#2 - Проектирование и создание JavaScript freamwork'а"
-            }
-        ],
-        "imagePath": "mikita_manko.jpg"
-    },
-    {
-        "name": "Nickolay Lavrynovich",
-        "talks": [
-            {
-                "title": "RS#10 - Paper.js"
-            }
-        ],
-        "imagePath": "nickolay_lavrynovich.jpg"
-    },
-    {
-        "name": "Pavel Nasovich",
-        "talks": [
-            {
-                "title": "RSW#2 - WebSocket Challenge"
-            }
-        ],
-        "imagePath": "pavel_nasovich.jpg"
-    },
-    {
-        "name": "Serge Barysiuk",
-        "talks": [
-            {
-                "title": "RS#6 - История одного стартапа: Angular.js и как мы его готовим"
-            }
-        ],
-        "imagePath": "serge_barysiuk.jpg"
-    },
-    {
-        "name": "Siarhei Melnik",
-        "talks": [
-            {
-                "title": "RS#10 - NewsDugest"
-            }
-        ],
-        "imagePath": "siarhei_melnik.jpg"
-    },
-    {
-        "name": "Stanislau Zubovich",
-        "talks": [
-            {
-                "title": "RS#4 - Knock-Knock Flexbox"
-            }
-        ],
-        "imagePath": "stanislau_zubovich.jpg"
-    },
-    {
-        "name": "Uladzimir Abramchuk",
-        "talks": [
-            {
-                "title": "RS#9 - Лямбда-исчисление"
-            }
-        ],
-        "imagePath": "uladzimir_abramchuk.jpg"
-    },
-    {
-        "name": "Vitaly Friedman",
-        "talks": [
-            {
-                "title": "RS Q&A #1 - Responsive Web Design"
-            }
-        ],
-        "imagePath": "vitaly_friedman.jpg"
-    },
-    {
-        "name": "Vladimir Petriko",
-        "talks": [
-            {
-                "title": "RS#3 - Chrome Remote Debugging Protocol"
-            }
-        ],
-        "imagePath": "vladimir_petriko.jpg"
-    },
-    {
-        "name": "Yauhen Ivashkevich",
-        "talks": [
-            {
-                "title": "RS#6 - Node.js Production on Windows Azure"
-            }
-        ],
-        "imagePath": "yauhen_ivashkevich.jpg"
-    },
-    {
-        "name": "Yauheni Leichanok",
-        "talks": [
-            {
-                "title": "RS#1 - Fast Websites"
-            }
-        ],
-        "imagePath": "yauheni_leichanok.jpg"
-    }
+	{
+		"name": "Alexander Gerasimov",
+		"ruName": "Александр Герасимов",
+		"talks": [
+			{
+				"title": "RS#2 - Front-end project assets management"
+			},
+			{
+				"title": "RS#3 - SpainJS"
+			},
+			{
+				"title": "RSW#1 - TDD"
+			},
+			{
+				"title": "RSW#2 - WebSocket Challenge"
+			},
+			{
+				"title": "RS#4 - Testing JavaScript Applications"
+			},
+			{
+				"title": "RS#8 - Keep calm & attend events: LondonJs"
+			}
+		],
+		"imagePath": "alexander_gerasimov.jpg"
+	},
+	{
+		"name": "Siarhei Mikhailau",
+		"ruName": "Сергей Михайлов",
+		"talks": [
+			{
+				"title": "RS#2 - HTML-coder never dies"
+			},
+			{
+				"title": "RS#3 - Graceful Degradation and Progressive Enhancement"
+			},
+			{
+				"title": "RS#4 - Popups - breaking the habits"
+			},
+			{
+				"title": "RS#6 - Hello, Data Binding!"
+			}
+		],
+		"imagePath": "siarhei_mikhailau.jpg"
+	},
+	{
+		"name": "Dzmitry Varabei",
+		"ruName": "Дмитрий Воробей",
+		"talks": [
+			{
+				"title": "RS#1 - Chrome DevTools"
+			},
+			{
+				"title": "RS#3 - SpainJS"
+			},
+			{
+				"title": "RS#3 - Chrome Internals: Paint and Composition"
+			}
+		],
+		"imagePath": "dzmitry_varabei.jpg"
+	},
+	{
+		"name": "Andre Gloukhmantchouk",
+		"ruName": "Андрей Глухманчук",
+		"talks": [
+			{
+				"title": "RS#2 - Front-end project assets management"
+			},
+			{
+				"title": "RS#3 - SpainJS"
+			},
+			{
+				"title": "RS#5 - Going To Interview: Best Practices"
+			}
+		],
+		"imagePath": "andre_gloukhmantchouk.jpg"
+	},
+	{
+		"name": "Alex Karlovich",
+		"ruName": "Александр Карлович",
+		"talks": [
+			{
+				"title": "RS#2 - <video> хроника"
+			},
+			{
+				"title": "RS#3 - SpainJS"
+			},
+			{
+				"title": "RS#8 - Stylish Stylus"
+			}
+		],
+		"imagePath": "alex_karlovich.jpg"
+	},
+	{
+		"name": "Konstantin Krivlenia",
+		"ruName": "Константин Кривленя",
+		"talks": [
+			{
+				"title": "RS#4 - JavaScript Must Die!"
+			},
+			{
+				"title": "RS#7 - JavaScript Virtual Machines"
+			},
+			{
+				"title": "RS#10 - DOM sweet DOOM"
+			}
+		],
+		"imagePath": "konstantin_krivlenia.jpg"
+	},
+	{
+		"name": "Aliaksei Shitkin",
+		"ruName": "Алексей Шиткин",
+		"talks": [
+			{
+				"title": "RS#1 - Web Components"
+			},
+			{
+				"title": "RS#2 - WebComponents in action"
+			},
+			{
+				"title": "RS#9 - JavaScript Linters"
+			}
+		],
+		"imagePath": "aliaksei_shitkin.jpg"
+	},
+	{
+		"name": "Dzmitry Gerasimov",
+		"ruName": "Дмитрий Герасимов",
+		"talks": [
+			{
+				"title": "RS#4 - Testing JavaScript Applications"
+			},
+			{
+				"title": "RSW#1 - TDD"
+			}
+		],
+		"imagePath": "dzmitry_gerasimov.jpg"
+	},
+	{
+		"name": "Kirill Kukhovets",
+		"ruName": "Кирилл Куховец",
+		"talks": [
+			{
+				"title": "RS#5 -Welcome to Node.js"
+			},
+			{
+				"title": "RS#7 - Data processing with ql.io"
+			}
+		],
+		"imagePath": "kirill_kukhovets.jpg"
+	},
+	{
+		"name": "Andrei Palchys",
+		"ruName": "Андрей Пальчис",
+		"talks": [
+			{
+				"title": "RS#3 - Asm.js"
+			},
+			{
+				"title": "RS#7 - WebKit Project Overview"
+			}
+		],
+		"imagePath": "andrei_palchys.jpg"
+	},
+	{
+		"name": "Viktor Khomyakov",
+		"ruName": "Виктор Хомяков",
+		"talks": [
+			{
+				"title": "RS#5 - Seamless and partial page reload"
+			},
+			{
+				"title": "RS#11 - Интеграция React в проект на RequareJS"
+			}
+		],
+		"imagePath": "viktor_khomyakov.jpg"
+	},
+	{
+		"name": "Uladzimir Abramchuk",
+		"ruName": "Владимир Абрамчук",
+		"talks": [
+			{
+				"title": "RS#9 - Лямбда-исчисление"
+			}
+		],
+		"imagePath": "uladzimir_abramchuk.jpg"
+	},
+	{
+		"name": "Artsiom Aliakseyenka",
+		"ruName": "Артём Алексеенко",
+		"talks": [
+			{
+				"title": "RS#11 - Memory Profiling"
+			}
+		],
+		"imagePath": "artsiom_aliakseyenka.jpg"
+	},
+	{
+		"name": "Kiryl Baranosnik",
+		"ruName": "Кирилл Бараношник",
+		"talks": [
+			{
+				"title": "RS#5 -Taming AngularJs: A Directive Lifecycle"
+			}
+		],
+		"imagePath": "kiryl_baranosnik.jpg"
+	},
+	{
+		"name": "Serge Barysiuk",
+		"ruName": "Сергей Борисюк",
+		"talks": [
+			{
+				"title": "RS#6 - История одного стартапа: Angular.js и как мы его готовим"
+			}
+		],
+		"imagePath": "serge_barysiuk.jpg"
+	},
+	{
+		"name": "Kiryl Verkhovin",
+		"ruName": "Кирилл Верховин",
+		"talks": [
+			{
+				"title": "RSW#3 - Обработки и визуализация данных с помощью библиотеки D3.js на примере создания интерактивной карты Беларуси"
+			}
+		],
+		"imagePath": "kiryl_verkhovin.jpg"
+	},
+	{
+		"name": "Dmitry Dudin",
+		"ruName": "Дмитрий Дудин",
+		"talks": [
+			{
+				"title": "RS#9 - Экзотические и полезные web API"
+			}
+		],
+		"imagePath": "dmitry_dudin.jpg"
+	},
+	{
+		"name": "Evgeny Zarya",
+		"ruName": "Евгений Заря",
+		"talks": [
+			{
+				"title": "RS#4 - JavaScript: Non-classical inheritance"
+			}
+		],
+		"imagePath": "evgeny_zarya.jpg"
+	},
+	{
+		"name": "Aleh Zasypkin",
+		"ruName": "Олег Засыпкин",
+		"talks": [
+			{
+				"title": "RS Q&A #2 - How Mozilla Works"
+			}
+		],
+		"imagePath": "aleh_zasypkin.jpg"
+	},
+	{
+		"name": "Stanislau Zubovich",
+		"ruName": "Станислав Зубович",
+		"talks": [
+			{
+				"title": "RS#4 - Knock-Knock Flexbox"
+			}
+		],
+		"imagePath": "stanislau_zubovich.jpg"
+	},
+	{
+		"name": "Yauhen Ivashkevich",
+		"ruName": "Евгений Ивашкевич",
+		"talks": [
+			{
+				"title": "RS#6 - Node.js Production on Windows Azure"
+			}
+		],
+		"imagePath": "yauhen_ivashkevich.jpg"
+	},
+	{
+		"name": "Nickolay Lavrynovich",
+		"ruName": "Николай Лавринович",
+		"talks": [
+			{
+				"title": "RS#10 - Paper.js"
+			}
+		],
+		"imagePath": "nickolay_lavrynovich.jpg"
+	},
+	{
+		"name": "Mikhail Larchanka",
+		"ruName": "Михаил Ларченко",
+		"talks": [
+			{
+				"title": "RS#9 - Google Glass"
+			}
+		],
+		"imagePath": "mikhail_larchanka.jpg"
+	},
+	{
+		"name": "Yauheni Leichanok",
+		"ruName": "Евгений Лейченок",
+		"talks": [
+			{
+				"title": "RS#1 - Fast Websites"
+			}
+		],
+		"imagePath": "yauheni_leichanok.jpg"
+	},
+	{
+		"name": "Mikita Manko",
+		"ruName": "Никита Манько",
+		"talks": [
+			{
+				"title": "RS#2 - Проектирование и создание JavaScript freamwork'а"
+			}
+		],
+		"imagePath": "mikita_manko.jpg"
+	},
+	{
+		"name": "Siarhei Melnik",
+		"ruName": "Сергей Мельник",
+		"talks": [
+			{
+				"title": "RS#10 - NewsDugest"
+			}
+		],
+		"imagePath": "siarhei_melnik.jpg"
+	},
+	{
+		"name": "Egor Miasnikov",
+		"ruName": "Егор Мясников",
+		"talks": [
+			{
+				"title": "RS#9 - Google Glass"
+			}
+		],
+		"imagePath": "egor_miasnikov.jpg"
+	},
+	{
+		"name": "Pavel Nasovich",
+		"ruName": "Павел Насович",
+		"talks": [
+			{
+				"title": "RSW#2 - WebSocket Challenge"
+			}
+		],
+		"imagePath": "pavel_nasovich.jpg"
+	},
+	{
+		"name": "Vladimir Petriko",
+		"ruName": "Владимир Петрико",
+		"talks": [
+			{
+				"title": "RS#3 - Chrome Remote Debugging Protocol"
+			}
+		],
+		"imagePath": "vladimir_petriko.jpg"
+	},
+	{
+		"name": "Andrew Fan",
+		"ruName": "Андрей Фан",
+		"talks": [
+			{
+				"title": "RS#1 - Introduction to Ember"
+			}
+		],
+		"imagePath": "andrew_fan.jpg"
+	},
+	{
+		"name": "Vitaly Friedman",
+		"ruName": "Виталий Фридман",
+		"talks": [
+			{
+				"title": "RS Q&A #1 - Responsive Web Design"
+			}
+		],
+		"imagePath": "vitaly_friedman.jpg"
+	},
+	{
+		"name": "Maksim Chemerisuk",
+		"ruName": "Максим Чемерисюк",
+		"talks": [
+			{
+				"title": "RS#8 - Some fresh ideas from better-dom"
+			}
+		],
+		"imagePath": "maksim_chemerisuk.jpg"
+	},
+	{
+		"name": "Denis Shamgin",
+		"ruName": "Денис Шамгин",
+		"talks": [
+			{
+				"title": "RS#1 - Introduction to Angular.js"
+			}
+		],
+		"imagePath": "denis_shamgin.jpg"
+	},
+	{
+		"name": "Dzianis Sheka",
+		"ruName": "Денис Шеко",
+		"talks": [
+			{
+				"title": "RS#10 - Назад в будущее, или мобильная разработка сегодня"
+			}
+		],
+		"imagePath": "dzianis_sheka.jpg"
+	}
 ]
