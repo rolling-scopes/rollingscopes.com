e83ac327f91b7aecd1e917a2803041df7e54e730
Dzmtiry Varabei <dzmitry.varabei@gmail.com>
2014-10-16 16:06:20 +0300

    create separate conference folder

---
+++ conference/scripts/main.js
@@ -0,0 +1,30 @@
+window.onload = function(){
+    var el = document.getElementById("nav");
+    var navBtn = document.getElementById("nav-btn");
+
+    window.toggleClass = function() {
+        if (el.className == "vertical-mode-closed") {
+            el.className = "vertical-mode";
+            navBtn.innerHTML = 'x';
+        } else {
+            el.className = "vertical-mode-closed";
+            navBtn.innerHTML = '&#x2261;';
+        }
+    }
+
+    window.onresize = function () {
+        el.className = "vertical-mode-closed";
+        navBtn.innerHTML = '&#x2261;';
+    }
+
+    nav.onclick = function () {
+        el.className = "vertical-mode-closed";
+        navBtn.innerHTML = '&#x2261;';
+    }
+}
+
+if ("ontouchstart" in window){
+    document.body.className += "isTouch";
+} else {
+    document.body.className += "isDesktop";
+}
