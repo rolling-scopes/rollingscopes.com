e02fc73c23791574aa377c0695a2f2b086b3fb33
Dzmtiry Varabei <dzmitry.varabei@gmail.com>
2014-10-17 11:29:52 +0300

    use 'rs_dist' as default build destination folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function (grunt) {
   // Configurable paths
   var config = {
     app: 'app',
-    dist: 'dist'
+    dist: 'rs_dist'
   };
 
   // Define the configuration for all the tasks
