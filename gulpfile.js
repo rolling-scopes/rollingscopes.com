78b35bad651307e6518714fa91087c088a747125
Andrei Volchenko <dev.volchenko@gmail.com>
2015-04-07 14:38:46 +0300

    add useref task

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 var gulp        = require('gulp');
+var gulpif      = require('gulp-if');
+
 var minimist    = require('minimist');
 var gulpOptions = require('./gulp-options');
 
@@ -7,6 +9,11 @@ var pngquant    = require('imagemin-pngquant');
 
 var options = minimist(process.argv.slice(2), gulpOptions);
 options.dest = options.src + 'Bin';
+
+var useref      = require('gulp-useref');
+var uglify      = require('gulp-uglify');
+var minifyCss   = require('gulp-minify-css');
+var htmlmin     = require('gulp-htmlmin');
 
 gulp.task('imagemin', function () {
   return gulp.src(
@@ -21,4 +28,19 @@ gulp.task('imagemin', function () {
   .pipe(
     gulp.dest(options.dest + '/images')
   );
+});
+
+gulp.task('useref', function () {
+  var assets = useref.assets();
+
+  return gulp.src(
+    options.src + '/*.html'
+  )
+  .pipe(assets)
+  .pipe(gulpif('*.js', uglify()))
+  .pipe(gulpif('*.css', minifyCss()))
+  .pipe(assets.restore())
+  .pipe(useref())
+  .pipe(htmlmin({collapseWhitespace: true}))
+  .pipe(gulp.dest(options.dest));
 });
