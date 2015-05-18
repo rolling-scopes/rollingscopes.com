867eaf4a3db9f479c47596765b8f69cdadb5bb16
Rolling Scopes <rolling.scopes@gmail.com>
2015-05-18 17:17:24 +0300

    remove aws related files

--- hooks/after-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-sudo -i
-cd /webapp
-npm i
-cd /webapp/school
-bower i --allow-root
-cd /webapp
-gulp
--- hooks/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-sudo killall nodejs
-cd /webapp
-sudo forever start --minUptime 1000 --spinSleepTime 1000 service.js
