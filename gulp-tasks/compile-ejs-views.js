abd13bdab4fd5aa6d3803d4eeabd4634f7e2cfe0
Andrei Volchenko <volchenko.andrei@gmail.com>
2015-09-14 22:28:23 +0300

    add active state for nav items

--- gulp-tasks/compile-ejs-views.js
+++ gulp-tasks/compile-ejs-views.js
@@ -3,17 +3,19 @@ var ejs = require('ejs');
 
 module.exports = function (datasets) {
   return t2.obj(function (file, encoding, done) {
+    var data;
     var pageName = file.path
       .replace(file.base, '')
       .replace('.ejs', '');
 
     data = datasets[pageName];
     data.filename = file.path;
+    data.pageName = pageName;
 
     file.contents = new Buffer(ejs.render(file.contents.toString('utf-8'), data));
     file.path = file.path.replace(/\.ejs$/, '.html');
 
     this.push(file);
     done();
-  })
-}
+  });
+};
