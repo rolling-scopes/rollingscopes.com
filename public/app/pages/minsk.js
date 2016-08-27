f8439fa0348aec2addd98b60ac8632e284dfbee4
Kseniya Varabei <kseniya.varabei@gmail.com>
2016-08-27 13:55:33 +0300

    add RS#29 agenda

--- public/app/pages/minsk.js
+++ public/app/pages/minsk.js
@@ -4,7 +4,8 @@ var loadEvents   = require('../data/loaders/archive-loader');
 var events = loadEvents()
   .filter((event) => {
     return event.location.indexOf('г. Брест') === -1 &&
-      event.location.indexOf('г. Гомель') === -1;
+      event.location.indexOf('г. Гомель') === -1 &&
+      event.location.indexOf('Krakow') === -1;
   });
 
 var currentEvent = events[0];
